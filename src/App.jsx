7d8ca5c619f994d178e0178707f7585c212ac7b9
atishay5845 <atishaysodhiya5845@gmail.com>
2026-07-25 22:24:21 +0530

    added tailwind,daizy ui navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,12 @@ function App() {
 
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-base-300 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">👨‍💻DevCircle</a>
   </div>
   <div className="flex gap-2">
-    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    <div className="dropdown dropdown-end">
+    <div className="dropdown dropdown-end mx-5">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img
@@ -38,7 +37,7 @@ function App() {
     </div>
   </div>
 </div>
-<h1>devCircle</h1>
+<h1>d</h1>
     </>
   )
 }
