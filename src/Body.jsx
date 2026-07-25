eb6a8dbd8a50cf0b664ff4f4ecb2d4ddfe011df9
atishay5845 <atishaysodhiya5845@gmail.com>
2026-07-26 00:42:57 +0530

    added footer component

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-
+import Footer from "./Footer";
 const Body = () => {
   return (
     <div>
       <Navbar />
       <Outlet/>
        {/* //any child will be rendered */}
+
+      <Footer/>
     </div>
   )
 }
