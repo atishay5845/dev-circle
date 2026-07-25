d2d0fc46a349509781e1cc1e6ba2db655fdf1de6
atishay5845 <atishaysodhiya5845@gmail.com>
2026-07-25 23:59:31 +0530

    setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Navbar from './Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
-    <h1>devCircle</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<div>base page</div>} />
+          <Route path="/login" element={<div>login page</div>} />
+          <Route path="/test" element={<div>test page</div>} />
+        </Routes>
+      </BrowserRouter>
+      <Navbar />
+      <h1>devCircle</h1>
     </>
   )
 }
